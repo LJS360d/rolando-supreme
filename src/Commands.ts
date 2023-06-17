865e0a0d463a9d4e8ff0fa8555d4416f03c0f2dd
LUCA LENCIONI <luca.lencioni09@gmail.com>
2023-06-17 20:20:28 +0200

    Release 0.2.4 Componentized Commands Refactored Interactions to a Switch Case statement

---
+++ src/Commands.ts
@@ -0,0 +1,42 @@
+export const commands = [
+    {
+      name: 'irlfact',
+      description: 'Replies with a random Real Life fact',
+    },
+    {
+      name: 'catfact',
+      description: 'Replies with a random cat fact'
+    },
+    {
+      name: 'gif',
+      description: 'Replies with a gif from the ones it has learned COULD BE NSFW!'
+    },
+    {
+      name: 'image',
+      description: 'Replies with an image from the ones it has learned COULD BE NSFW!'
+    },
+    {
+      name: 'video',
+      description: 'Replies with an video/youtube link from the ones it has learned COULD BE NSFW!'
+    },
+    {
+      name: 'providetraining',
+      description: 'Memorizes all the messages of the SERVER and uses them as training data',
+    },
+    {
+      name: 'replyrate',
+      description: 'Shows the current reply rate',
+    },
+    {
+      name: 'setreplyrate',
+      description: 'Sets the rate at which the bot will reply',
+      options: [
+        {
+          name: 'rate',
+          description: "Probability of 1/rate | 1=always reply | 0=never reply unless pinged",
+          type: 4, //Integer type
+          required: true,
+        }
+      ]
+    }
+  ];
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import {
 } from 'discord.js';
 
 import { startAdminServer } from './AdminServer';
+import { commands } from './Commands';
 import { DataRetriever } from './DataRetriever';
 import {
   chainsMap,
@@ -37,55 +38,6 @@ const options = {
 export const client = new Client(options);
 const dataRetriever = new DataRetriever()
 
-const commands = [
-  {
-    name: 'irlfact',
-    description: 'Replies with a random Real Life fact',
-  },
-  {
-    name: 'catfact',
-    description: 'Replies with a random cat fact'
-  },
-  {
-    name: 'gif',
-    description: 'Replies with a gif from the ones it has learned COULD BE NSFW!'
-  },
-  {
-    name: 'image',
-    description: 'Replies with an image from the ones it has learned COULD BE NSFW!'
-  },
-  {
-    name: 'video',
-    description: 'Replies with an video/youtube link from the ones it has learned COULD BE NSFW!'
-  },
-  {
-    name: 'providetraining',
-    description: 'Memorizes all the messages of the SERVER and uses them as training data',
-  },
-  {
-    name: 'replyrate',
-    description: 'Shows the current reply rate',
-  },
-  {
-    name: 'setreplyrate',
-    description: 'Sets the rate at which the bot will reply',
-    options: [
-      {
-        name: 'rate',
-        description: "Probability of 1/rate | 1=always reply | 0=never reply unless pinged",
-        type: 4, //Integer type
-        required: true,
-      }
-    ]
-  }
-];
-async function refreshCommands(): Promise<void> {
-  try {
-    console.log('Started refreshing application (/) commands.');
-    await client.application?.commands.set(commands);
-  } catch (error) { console.error(error) }
-}
-
 client.on('ready', () => {
   refreshCommands().then(() => { console.log('Successfully reloaded application (/) commands.') })
   console.log(`Logged in as ${client.user!.tag}!`);
@@ -105,85 +57,84 @@ client.on('ready', () => {
 
   })
   console.log(`Started ${chainsMap.size} Chains`);
-
-});
-
+  async function refreshCommands(): Promise<void> {
+    try {
+      console.log('Started refreshing application (/) commands.');
+      await client.application?.commands.set(commands);
+    } catch (error) { console.error(error) }
+  }
+})
 client.on('guildCreate', (guild: Guild) => {
   chainsMap.set(guild.id, new MarkovChain())
-  console.log(`Joined a new guild: ${guild.name} (ID: ${guild.id})`);
-  console.log('Guild member count:', guild.memberCount);
-  console.log('Guild owner:', guild.members.cache.get(guild.ownerId));
-
-});
+  guild.systemChannel.send(`Sup, i'm Rolando. \nRun \`/providetraining\` to actually make me do stuff.\nThe more messages there are in the server, the more it will make me intelligent.`)
+})
 client.on('interactionCreate', async function (interaction: ChatInputCommandInteraction) {
-  if (interaction.commandName === 'irlfact') {
-    (await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', {
-      headers: { 'Accept': 'application/json' }
-    })).json().then(async res => {
+  const chain = chainsMap.get(interaction.guildId);
+  switch (interaction.commandName) {
+    case 'irlfact':
+      (await fetch('https://uselessfacts.jsph.pl/api/v2/facts/random', {
+        headers: { 'Accept': 'application/json' }
+      })).json().then(async res => {
+        await interaction.reply(res.text);
+      }).catch(error => {
+        console.error(error);
+      });
+      break;
 
-      await interaction.reply(res.text);
+    case 'catfact':
+      (await fetch('https://meowfacts.herokuapp.com/', {
+        headers: { 'Accept': 'application/json' }
+      })).json().then(async res => {
+        await interaction.reply(res.data[0]);
+      }).catch(error => {
+        console.error(error);
+      });
+      break;
 
-    }).catch(error => {
-      console.error(error)
-    })
-  }
+    case 'providetraining':
+      if (!dataRetriever.fileManager.guildHasPreviousData(interaction.guild.id)) {
+        interaction.reply(`<@${interaction.user.id}> Started Fetching messages.\nWill send another message when I'm done\nEstimated Time: \`1 Minute per every 4000 Messages in the Server\`\nThis might take a while...`);
+        const start = Date.now();
+        await dataRetriever.fetchAndStoreAllMessagesInGuild(interaction.guild).then(() => {
+          const runtime = new Date(Date.now() - start);
+          const formattedTime = `${runtime.getMinutes()}m ${runtime.getSeconds()}s`;
+          interaction.channel.send(`<@${interaction.user.id}> Finished Fetching training data!\nTime Passed:\`${formattedTime}\``);
+          chainsMap.get(interaction.guild.id).provideData(dataRetriever.fileManager.getPreviousTrainingDataForGuild(interaction.guild.id));
+        });
+      } else {
+        await interaction.reply(`I already have training data for this server`);
+      }
+      break;
 
-  if (interaction.commandName === 'catfact') {
-    (await fetch('https://meowfacts.herokuapp.com/', {
-      headers: { 'Accept': 'application/json' }
-    })).json().then(async res => {
+    case 'setreplyrate':
+      if (!chain) return;
+      chain.replyRate = interaction.options.getInteger('rate');
 
-      await interaction.reply(res.data[0]);
+      if (chain.replyRate > 1) {
+        await interaction.reply(`Set reply rate to ${chain.replyRate}`);
+      } else if (chain.replyRate === 1) {
+        await interaction.reply(`Ok, I will always reply`);
+      } else if (chain.replyRate === 0) {
+        await interaction.reply(`Ok, I won't reply to anybody`);
+      }
+      break;
 
-    }).catch(error => {
-      console.error(error)
-    })
-  }
+    case 'replyrate':
+      if (!chain) return;
+      await interaction.reply(`The reply rate is currently set to ${chain.replyRate}\nUse \`/setreplyrate\` to change it`);
+      break;
 
-  if (interaction.commandName === 'providetraining') {
-    if (!dataRetriever.fileManager.guildHasPreviousData(interaction.guild.id)) {
-      interaction.reply(`<@${interaction.user.id}> Started Fetching messages.\nWill send another message when i'm done\nEstimated Time: \`1 Minute per every 4000 Messages in the Server\`\nThis might take a while...`)
-      const start = Date.now();
-      await dataRetriever.fetchAndStoreAllMessagesInGuild(interaction.guild).then(() => {
-        const runtime = new Date(Date.now() - start);
-        const formattedTime = `${runtime.getMinutes()}m ${runtime.getSeconds()}s`;
-        interaction.channel.send(`<@${interaction.user.id}> Finished Fetching training data!\nTime Passed:\`${formattedTime}\``)
-        chainsMap.get(interaction.guild.id)
-          .provideData(dataRetriever.fileManager
-            .getPreviousTrainingDataForGuild(interaction.guild.id))
-      })
-    } else {
-      await interaction.reply(`I already have training data for this server`)
-    }
-  }
+    case 'gif':
+      await interaction.reply(await chain.getGif());
+      break;
 
-  if (interaction.commandName === 'setreplyrate') {
-    const chain = chainsMap.get(interaction.guildId)
-    if (!chain) return
-    chain.replyRate = interaction.options.getInteger('rate')
+    case 'image':
+      await interaction.reply(await chain.getImage());
+      break;
 
-    if (chain.replyRate > 1)
-      await interaction.reply(`Set reply rate to ${chain.replyRate}`)
-    if (chain.replyRate === 1)
-      await interaction.reply(`Ok i will always reply`)
-    if (chain.replyRate === 0)
-      await interaction.reply(`Ok i won't reply to anybody`)
-  }
-
-  if (interaction.commandName === 'replyrate') {
-    const chain = chainsMap.get(interaction.guildId)
-    if (!chain) return
-    await interaction.reply(`The reply rate is currently set to ${chain.replyRate}\nUse \`/setreplyrate\` to change it`) 
-  }
-
-  if (interaction.commandName === 'gif') {
-    await interaction.reply(await chainsMap.get(interaction.guild.id).getGif())
-  }
-  if (interaction.commandName === 'image') {
-    await interaction.reply(await chainsMap.get(interaction.guild.id).getImage())
-  }
-  if (interaction.commandName === 'video') {
-    await interaction.reply(await chainsMap.get(interaction.guild.id).getVideo())
+    case 'video':
+      await interaction.reply(await chain.getVideo());
+      break;
   }
 });
 
@@ -212,33 +163,16 @@ client.on('messageCreate', async (msg: Message) => {
       await msg.channel.send(reply)
   }
 })
+
 client.login(TOKEN);
 startAdminServer()
 
 process.on('SIGINT', async () => {
   console.log('Received SIGINT signal. Shutting down gracefully...');
-  // Perform any necessary cleanup operations here
-
   process.exit(0)
-});
+})
 
 process.on('SIGTERM', async () => {
   console.log('Received SIGTERM signal. Shutting down gracefully...');
-  // Perform any necessary cleanup operations here
   process.exit(0)
-});
-
-/* async function sendShutdownNotif() {
-  const axios = require("axios");
-  const webhook = process.env['WEBHOOK']
-  return new Promise((resolve, reject) => {
-    axios.post(webhook, { content: "Rolando Has Shut down", username: "Rolando" })
-      .then(() => {
-        resolve(0)
-      })
-      .catch((err: Error) => {
-        console.error(err)
-        reject(1)
-      })
-  })
-}*/
+})
